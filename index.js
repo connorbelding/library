a0e988e6f5fde3b2424b309c9cabf8270b1ea2cc
connorbelding <the_connn@hotmail.com>
2022-10-19 20:05:15 -0400

    Added updateBooksDisplay, removeAllChildren funcs

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("test");
+const addBookBtn = document.getElementById("add-book-btn");
+const booksContainer = document.getElementById("books");
+
+const myLibrary = [];
+
+function Book({ name, author, pages, read }) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  removeAllChildren(booksContainer);
+  updateBooksDisplay();
+}
+
+function updateBooksDisplay() {
+  myLibrary.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("book");
+    div.textContent = `${book.name} / ${book.author} / ${book.pages} pages / ${
+      book.read ? "read" : "unread"
+    }`;
+    booksContainer.appendChild(div);
+  });
+}
+
+function removeAllChildren(node) {
+  while (node.hasChildElements) {
+    node.removeChild(node.lastChild);
+  }
+}
